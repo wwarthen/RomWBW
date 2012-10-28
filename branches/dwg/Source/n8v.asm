da008af7165c567c8df1b5828e1470bb8e735d68
doug <doug@1438685a-98c7-b363-4179-13d573978243>
2012-10-28 13:03:26 +0000

    best so far

--- branches/dwg/Source/n8v.asm
+++ branches/dwg/Source/n8v.asm
@@ -1,3 +1,4 @@
+; ../RomWBW/Source/n8v.asm 10/28/2012 dwg - add n8v_modes
 ; ../RomWBW/Source/n8v.asm 10/27/2012 dwg - begin enhancement
 
 
@@ -115,6 +116,72 @@ N8V_VDASCR:
 	XOR	A
 	RET
 ;
+
+N8V_MODES:
+	; outp(CMDP,80);
+	; outp(CMDP,129);
+	RET
+
+N8V_PNT:
+	; outp(CMDP,0);
+	; outp(CMDP,130);
+	RET
+
+N8V_PGT:
+	; outp(CMDP,1);
+	; outp(CMDP,132);
+	RET
+
+N8V_COLORS:
+	; outp(CMDP,240);
+	; outp(CMDP,135);
+	RET
+
+N8V_LOADCHARS:
+	; out(CMDP,0);
+	; out(CMDP,72);
+	; index=0;
+	; for(c=0;c<256;c++P) {
+	; 	for(d=0;d<8;d++) {
+	;		out(DATAP,charset[index++]);
+	;	}
+	; }
+	RET
+
+N8V_FILL:
+	; out(CMDP,0);
+	; out(CMDP,64);
+	; d=0;
+	; for(c=0;c<(40*24);c++) {
+	; 	out(DATAP,d);
+	;	d++;
+	;	if(128 == d) d=0;
+	; }
+	RET
+
+N8V_WRVRAM:
+	; vdp_wrvram(o)
+	; {
+	; 	byte1 = o & 255;
+	;	byte2 = (o >> 8) | 0x40;
+	;	out(CMDP,byte1);
+	;	out(CMDP,byte2);
+	; }
+	RET
+
+
+N8V_DISPLAY:
+	; vdp_display(line,column,string)
+	; {
+	;	vdp_wrvram(GUTTER+(line*40)+column);
+	;	for(index=0;index<strlen(string);index++) {
+	;		out(DATAP,string[index]);
+	;	}
+	; }	
+	RET
+
+
+
 ;__________________________________________________________________________________________________
 ; IMBED COMMON PRALLEL PORT KEYBOARD DRIVER
 ;__________________________________________________________________________________________________
