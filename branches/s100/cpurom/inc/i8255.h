bf0a2d8d1c96c7bac7b1d6d8b4e1b7dc34916812
doug <doug@1438685a-98c7-b363-4179-13d573978243>
2013-01-28 13:15:42 +0000

    Complete submission of C firmware suite from ubios by douglas goodall

---
+++ branches/s100/cpurom/inc/i8255.h
@@ -0,0 +1,15 @@
+/*
+ * i8255.h Intel 8255      
+ *
+ */
+
+
+void uart_init(U8 baud);
+U8 uart_conin(void);
+void uart_conout(U8 data);
+
+/*
+ *
+ * eof - i8255.h 
+ *
+ */
---
+++ branches/s100/cpurom/inc/portab.h
@@ -0,0 +1,12 @@
+/************/
+/* portab.h */
+/************/
+
+#define TRUE  1
+#define FALSE 0
+
+#define U8 unsigned char
+
+/******************/
+/* eof - portab.h */
+/******************/
