a020a155f62ec0756a7cc1f22c29d6ce0a9dae11
wayne <wayne@1438685a-98c7-b363-4179-13d573978243>
2013-01-30 05:48:04 +0000

    Merge trunk -> dgg

---
+++ branches/dgg/cpurom/inc/ns16550.h
@@ -0,0 +1,15 @@
+/*
+ * n16550.h National 16550 
+ *
+ */
+
+#define UART_DLAB 0x80
+#define UART_BAUD_9600 12
+#define UART_RDA       0x01
+#define UART_TBE       0x20
+
+/*
+ *
+ * eof - n16550.h 
+ *
+ */
---
+++ branches/dgg/cpurom/makeclean.bat
@@ -0,0 +1,3 @@
+wmake -f makefile.xp clean
+rem wmake -f makefile.xp
+dir rom
