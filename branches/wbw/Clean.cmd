c8cf6bee965b8b187416bbf9503daa2d7e799cbf
wayne <wayne@1438685a-98c7-b363-4179-13d573978243>
2013-04-10 16:47:22 +0000

    More tuning of apps build scripts

--- branches/wbw/Clean.cmd
+++ branches/wbw/Clean.cmd
@@ -1,5 +1,22 @@
 @echo off
+setlocal
+
+echo Cleaning Source...
 pushd Source
 call Clean.cmd
 popd
+
+echo Cleaning Apps\core...
+pushd Apps\core
+del /q *.*
+popd
+
+echo Cleaning Apps\crossdev...
+pushd Apps\crossdev
+call Clean.bat
+popd
+
+choice /m "Clean Output directory?"
+if errorlevel 2 goto :eof
+echo Cleaning Output...
 if exist Output\*.* del Output\*.* /Q
