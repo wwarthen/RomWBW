6a5253d54b6da422306492920f06ec8bcae02b12
wayne <wayne@1438685a-98c7-b363-4179-13d573978243>
2013-06-22 04:25:23 +0000

    Merge trunk -> dwg

--- branches/dwg/Apps/Source/applvers.h
+++ branches/dwg/Apps/Source/applvers.h
@@ -1,14 +1,14 @@
 /************************************/
-/* applvers.h dwg - 2.5.0.14 Beta   */
+/* applvers.h dwg - 2.5.0.15        */
 /************************************/
 
 #define A_RMJ	2
 #define A_RMN	5
 #define A_RUP	0
-#define A_RTP	14
+#define A_RTP	15
 
 #define A_MONTH	6
-#define A_DAY	9
+#define A_DAY	16
 #define A_YEAR	2013
 #define A_YR	13
 
