917ac728a2c460bb1addd4106802a90719adf508
wayne <wayne@1438685a-98c7-b363-4179-13d573978243>
2013-06-23 04:52:57 +0000

    Reintegrate wbw -> trunk

--- trunk/Apps/Source/applvers.h
+++ trunk/Apps/Source/applvers.h
@@ -5,10 +5,10 @@
 #define A_RMJ	2
 #define A_RMN	5
 #define A_RUP	0
-#define A_RTP	15
+#define A_RTP	16
 
 #define A_MONTH	6
-#define A_DAY	16
+#define A_DAY	23
 #define A_YEAR	2013
 #define A_YR	13
 
