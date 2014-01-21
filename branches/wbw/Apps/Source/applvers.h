e5a4fd7689b5fd0f80529263e7c688e2b9bc782f
wwarthen <wwarthen@1438685a-98c7-b363-4179-13d573978243>
2014-01-21 02:07:41 +0000

    Version 2.5.3

--- branches/wbw/Apps/Source/applvers.h
+++ branches/wbw/Apps/Source/applvers.h
@@ -1,16 +1,16 @@
 /************************************/
-/* applvers.h dwg - 2.5.2.18        */
+/* applvers.h dwg - 2.5.3.19        */
 /************************************/
 
 #define A_RMJ	2
 #define A_RMN	5
-#define A_RUP	2
-#define A_RTP	18
+#define A_RUP	3
+#define A_RTP	19
 
-#define A_MONTH	9
-#define A_DAY	29
-#define A_YEAR	2013
-#define A_YR	13
+#define A_MONTH	1
+#define A_DAY	20
+#define A_YEAR	2014
+#define A_YR	14
 
 /********************/
 /* eof - applvers.h */
