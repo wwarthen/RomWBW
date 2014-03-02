2e4eb2fb1bd36995cb12091af32d20c4873b665b
wwarthen <wwarthen@1438685a-98c7-b363-4179-13d573978243>
2014-03-02 02:37:34 +0000

    Reintegrate wbw -> trunk

--- Apps/Source/applvers.h
+++ Apps/Source/applvers.h
@@ -1,14 +1,14 @@
 /************************************/
-/* applvers.h dwg - 2.5.3.19        */
+/* applvers.h dwg - 2.5.4.20        */
 /************************************/
 
 #define A_RMJ	2
 #define A_RMN	5
-#define A_RUP	3
-#define A_RTP	19
+#define A_RUP	4
+#define A_RTP	20
 
-#define A_MONTH	1
-#define A_DAY	20
+#define A_MONTH	3
+#define A_DAY	1
 #define A_YEAR	2014
 #define A_YR	14
 
