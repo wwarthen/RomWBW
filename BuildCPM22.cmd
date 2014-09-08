74b80aaeeb14a0be709fa62255900673b6c20e41
wwarthen <wwarthen@1438685a-98c7-b363-4179-13d573978243>
2014-09-08 04:11:55 +0000

    Reintegrate wbw -> trunk

--- BuildCPM22.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-setlocal
-
-echo Building CP/M 2.2...
-cd CPM22
-call Build.cmd
--- Source/Clean.cmd
+++ Source/Clean.cmd
@@ -1,10 +1,10 @@
 @echo off
-if exist *.bin del *.bin
-if exist *.com del *.com
-if exist *.img del *.img
-if exist *.rom del *.rom
-if exist *.lst del *.lst
-if exist *.exp del *.exp
-if exist *.tmp del *.tmp
-if exist *.mrk del *.mrk
-if exist build.inc del build.inc
+setlocal
+
+pushd Apps && call Clean.cmd && popd
+pushd CPM22 && call Clean.cmd && popd
+pushd ZCPR && call Clean.cmd && popd
+pushd ZCPR-DJ && call Clean.cmd && popd
+pushd ZSDOS && call Clean.cmd && popd
+
+pushd BIOS && call Clean.cmd && popd
