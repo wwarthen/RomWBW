5b08693ba2c6d6d102e5a255013cf7a00623685b
Wayne Warthen <wwarthen@gmail.com>
2018-08-21 16:45:22 -0700

    Beep Support Updates

---
+++ Source/HBIOS/spk.asm
@@ -0,0 +1,36 @@
+;
+;======================================================================
+; I/O BIT DRIVER FOR CONSOLE BELL FOR SBC V2 USING BIT 0 OF RTC DRIVER
+;======================================================================
+;
+SPK_INIT:
+	CALL	NEWLINE			; FORMATTING
+	PRTS("SPK: IO=0x$")
+	LD	A,DSRTC_BASE
+	CALL	PRTHEXBYTE
+	CALL	SPK_BEEP
+	XOR		A
+	RET
+;
+SPK_BEEP:
+	PUSH	DE
+	PUSH	HL
+	LD	HL,400			; CYCLES OF TONE
+	;LD	B,%00000100		; D2 MAPPED TO Q0
+	LD	A,DSRTC_RESET
+	OR	%00000100		; D2 MAPPED TO Q0
+	LD	B,A
+SPK_BEEP1:
+	LD	A,B
+	OUT	(DSRTC_BASE),A
+	XOR	%00000100
+	LD	B,A
+	LD	DE,17
+	CALL	VDELAY
+	DEC	HL
+	LD	A,H
+	OR	L
+	JR	NZ,SPK_BEEP1
+	POP	HL
+	POP	DE
+	RET
