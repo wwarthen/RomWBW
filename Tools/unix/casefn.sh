c30c5a6437a120c254c8fc9560ab19b460b06b71
curt mayer <curt@zen-room.org>
2020-01-30 15:04:37 -0800

    build and diff fixes

--- Tools/unix/casefn.sh
+++ Tools/unix/casefn.sh
@@ -2,6 +2,7 @@
 # given a filename on the command line, echo the form of the file that 
 # actually can be opened.  this needs to do filesystem case shenanigans
 #
+n=0
 for infn in $* ; do
 	dir=$(dirname $infn)
 	lowname=$(basename $infn | tr '[A-Z]' '[a-z]')
@@ -10,7 +11,12 @@ for infn in $* ; do
 		cand=$(basename ./"$i" | tr '[A-Z]' '[a-z]')
 		if [ ./"$cand" = ./$lowname ] ; then
 			echo -n "$dir/$i "
+			((n++))
 		fi
 	done
 done
-echo
+if [ $n == 0 ] ; then
+	echo "nofile"
+else
+	echo
+fi
