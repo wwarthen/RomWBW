0445b3547cc828e85469f7661d8d56294deb67b8
Wayne Warthen <wwarthen@gmail.com>
2020-02-11 14:44:32 -0800

    Adjust for older PowerShell versions

--- Source/Images/Build.cmd
+++ Source/Images/Build.cmd
@@ -9,9 +9,9 @@ call Clean.cmd
 echo :
 echo : Building Floppy Disk Images...
 echo :
-call BuildFD.cmd cpm22 ..\cpm22\cpm_wbw
-call BuildFD.cmd zsdos ..\zsdos\zsys_wbw
-call BuildFD.cmd nzcom ..\zsdos\zsys_wbw
+call BuildFD.cmd cpm22 ..\cpm22\cpm_wbw.sys
+call BuildFD.cmd zsdos ..\zsdos\zsys_wbw.sys
+call BuildFD.cmd nzcom ..\zsdos\zsys_wbw.sys
 call BuildFD.cmd cpm3
 call BuildFD.cmd zpm3
 call BuildFD.cmd ws4
@@ -19,9 +19,9 @@ call BuildFD.cmd ws4
 echo :
 echo : Building Hard Disk Images...
 echo :
-call BuildHD.cmd cpm22 ..\cpm22\cpm_wbw
-call BuildHD.cmd zsdos ..\zsdos\zsys_wbw
-call BuildHD.cmd nzcom ..\zsdos\zsys_wbw
+call BuildHD.cmd cpm22 ..\cpm22\cpm_wbw.sys
+call BuildHD.cmd zsdos ..\zsdos\zsys_wbw.sys
+call BuildHD.cmd nzcom ..\zsdos\zsys_wbw.sys
 call BuildHD.cmd cpm3
 call BuildHD.cmd zpm3
 call BuildHD.cmd ws4
