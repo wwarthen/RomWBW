331cbce5c968605a89ee695f79aff7c9c34dd5d7
Wayne Warthen <wwarthen@gmail.com>
2020-02-19 11:08:39 -0800

    Update Clean.cmd

--- Source/BPBIOS/Clean.cmd
+++ Source/BPBIOS/Clean.cmd
@@ -8,6 +8,7 @@ if exist *.img del *.img
 if exist bp*.rel del bp*.rel
 if exist zcpr33*.rel del zcpr33*.rel
 if exist *.bak del *.bak
+if exist def-ww.lib del def-ww.lib
 
 setlocal & cd ZCPR33 && call Clean.cmd & endlocal
 setlocal & cd Z34RCP11 && call Clean.cmd & endlocal
