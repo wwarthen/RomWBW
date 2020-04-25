c3e78ec8f5a8415bba0b74bd22c75d448e299a46
Wayne Warthen <wwarthen@gmail.com>
2020-04-25 10:48:35 -0700

    Minor Cleanup

--- Source/Apps/Tune/Build.cmd
+++ Source/Apps/Tune/Build.cmd
@@ -6,8 +6,8 @@ set PATH=%TOOLS%\tasm32;%PATH%
 set TASMTABS=%TOOLS%\tasm32
 
 tasm -t180 -g3 -fFF -dWBW Tune.asm Tune.com Tune.lst
-tasm -t180 -g3 -fFF -dZX Tune.asm Tunezx.com Tune.lst
-tasm -t180 -g3 -fFF -dMSX Tune.asm Tunemsx.com Tune.lst
+tasm -t180 -g3 -fFF -dZX Tune.asm Tunezx.com Tunezx.lst
+tasm -t180 -g3 -fFF -dMSX Tune.asm Tunemsx.com Tunemsx.lst
 
 if errorlevel 1 goto :eof
 
