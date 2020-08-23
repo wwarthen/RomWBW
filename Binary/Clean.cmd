ad051867695ec4a2280830c421752826bddca916
Wayne Warthen <wwarthen@gmail.com>
2020-08-23 09:06:18 -0700

    Build Consistency Improvements

--- Binary/Clean.cmd
+++ Binary/Clean.cmd
@@ -7,7 +7,6 @@ if exist *.com del *.com
 if exist *.img del *.img
 if exist *.rom del *.rom
 if exist *.pdf del *.pdf
-if exist *.log del *.log
 if exist *.eeprom del *.eeprom
 
 setlocal & cd Apps && call Clean || exit /b 1 & endlocal
