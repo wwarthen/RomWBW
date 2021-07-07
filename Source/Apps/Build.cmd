3e8fe0c64cf8be4d281edb68c22a844df2c6d147
Wayne Warthen <wwarthen@gmail.com>
2021-07-06 17:28:08 -0700

    Add ppidetst to build
    - ppidetst was updated to TASM syntax and is now built automatically
    - completed the function within ppidetst to dynamically set the PPI base port address
    - cleaned up the formatting a bit

--- Source/Apps/Build.cmd
+++ Source/Apps/Build.cmd
@@ -22,6 +22,7 @@ call :asm Timer || goto :eof
 call :asm180 IntTest || goto :eof
 call :asm rtcds7 || goto :eof
 call :asm rtchb || goto :eof
+call :asm ppidetst || goto :eof
 
 zx Z80ASM -SYSGEN/F
 
