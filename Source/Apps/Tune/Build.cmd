3593a7fabd17dbd056dd98a79f58b0c6c9a89d33
Wayne Warthen <wwarthen@gmail.com>
2021-07-06 19:50:50 -0700

    Filename Case Problems
    In order to get the Linux/Mac and Windows builds to result in a binary identical result, I have changed the case of some filenames.  Windows and Linux handle directory sorting differently for upper/lower case in filenames.

--- Source/Apps/Tune/Build.cmd
+++ Source/Apps/Tune/Build.cmd
@@ -5,11 +5,11 @@ set TOOLS=../../../Tools
 set PATH=%TOOLS%\tasm32;%PATH%
 set TASMTABS=%TOOLS%\tasm32
 
-tasm -t180 -g3 -fFF -dWBW Tune.asm Tune.com Tune.lst
-tasm -t180 -g3 -fFF -dZX Tune.asm Tunezx.com Tunezx.lst
-tasm -t180 -g3 -fFF -dMSX Tune.asm Tunemsx.com Tunemsx.lst
+tasm -t180 -g3 -fFF -dWBW tune.asm tune.com tune.lst
+tasm -t180 -g3 -fFF -dZX tune.asm tunezx.com tunezx.lst
+tasm -t180 -g3 -fFF -dMSX tune.asm tunemsx.com tunemsx.lst
 
 if errorlevel 1 goto :eof
 
-copy /Y Tune*.com ..\..\..\Binary\Apps\
+copy /Y tune*.com ..\..\..\Binary\Apps\
 copy /Y Tunes\*.* ..\..\..\Binary\Apps\Tunes\
