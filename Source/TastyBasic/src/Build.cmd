b54438422573b3fe26bf8656b07aeacbf0846e0f
Wayne Warthen <wwarthen@gmail.com>
2021-11-01 20:25:33 -0700

    Sync Windows Build
    - Latest changes to Linux/MacOS build needed to be added to Windows build to restore binary identical build results.

--- Source/TastyBasic/src/Build.cmd
+++ Source/TastyBasic/src/Build.cmd
@@ -7,7 +7,10 @@ set PATH=%TOOLS%\tasm32;%PATH%
 
 set TASMTABS=%TOOLS%\tasm32
 
-tasm -80 -g3 -fFF -dROMWBW tastybasic.asm tastybasic.bin tastybasic.bin.lst
-tasm -80 -g3 -fFF -dCPM tastybasic.asm tastybasic.com tastybasic.com.lst
+# git@github.com:dimitrit/tastybasic.git; commit a86d7e7; (HEAD -> master, tag: v0.3.0) 
+set VER=v0.3.0
+
+tasm -80 -g3 -fFF -dROMWBW -d"VERSION \"%VER%\"" tastybasic.asm tastybasic.bin tastybasic.bin.lst
+tasm -80 -g3 -fFF -dCPM -d"VERSION \"%VER%\"" tastybasic.asm tastybasic.com tastybasic.com.lst
 
 copy /b /v tastybasic.com ..\..\..\Binary\Apps\tbasic.com
