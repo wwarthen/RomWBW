dd3dfff172150cda01391b8f2f0fcef7c9654a03
Wayne Warthen <wwarthen@gmail.com>
2021-11-17 14:01:26 -0800

    DMA Driver Speed Handling
    - Updated DMA driver to leave system speed as it originally found it.

--- Binary/Apps/Clean.cmd
+++ Binary/Apps/Clean.cmd
@@ -7,5 +7,6 @@ if exist *.doc del *.doc
 if exist *.hlp del *.hlp
 if exist Tunes\*.pt? del Tunes\*.pt?
 if exist Tunes\*.mym del Tunes\*.mym
+if exist Tunes\*.vgm del Tunes\*.vgm
 
 pushd Test && call Clean || exit /b 1 & popd
