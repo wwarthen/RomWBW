36b160f934a79da5ca64d506809a173e5d9f0838
Wayne Warthen <wwarthen@gmail.com>
2022-02-18 16:36:14 -0800

    Minor Cosmetic Cleanup
    Primarily fixed the Z280 config files to display the correct CPU speed in boot messages.

--- Source/ZZR/Build.cmd
+++ Source/ZZR/Build.cmd
@@ -1,7 +1,27 @@
 @echo off
 setlocal
 
-if not exist ..\..\Binary\RCZ280_nat_zzr.rom goto :eof
+set ROMFILE=..\..\Binary\RCZ280_nat_zzr.rom
+set ROMSIZE=262144
+
+if not exist %ROMFILE% goto :eof
+
+::
+:: The ROM image *must* be exactly 256K or the resulting disk
+:: image produced below will be invalid.  Check for the proper size.
+::
+
+call :filesize %ROMFILE%
+
+if "%FILESIZE%" neq "%ROMSIZE%" (
+  echo.
+  echo.
+  echo ERROR: "%ROMFILE%" is not exactly %ROMSIZE% bytes as required!!!
+  echo You must specify a ROMSIZE of "256" when building the ZZRCC ROM image.
+  echo.
+  echo.
+  exit /b 1
+)
 
 rem ..\..\Tools\srecord\srec_cat.exe ..\..\Binary\RCZ280_nat_zzr.rom -Binary -Exclude 0x5000 0x7000 zzr_romldr.hex -Intel -Output ..\..\Binary\RCZ280_nat_zzr.hex -Intel || exit /b
 
@@ -14,3 +34,9 @@ rem copy /b zzr_cfldr.bin + zzr_ptbl.bin + zzr_fill_1.bin + zzr_mon.bin + zzr_fi
 copy /b zzr_cfldr.bin + zzr_ptbl.bin + zzr_fill_1.bin + zzr_mon.bin + zzr_fill_2.bin + ..\..\Binary\RCZ280_nat_zzr.rom + zzr_fill_3.bin ..\..\Binary\hd1024_zzr_prefix.dat || exit /b
 
 copy /b ..\..\Binary\hd1024_zzr_prefix.dat + ..\..\Binary\hd1024_cpm22.img + ..\..\Binary\hd1024_zsdos.img + ..\..\Binary\hd1024_nzcom.img + ..\..\Binary\hd1024_cpm3.img + ..\..\Binary\hd1024_zpm3.img + ..\..\Binary\hd1024_ws4.img ..\..\Binary\hd1024_zzr_combo.img || exit /b
+
+goto :eof
+
+:filesize
+set FILESIZE=%~z1
+goto :eof
