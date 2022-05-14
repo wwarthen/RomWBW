fde0c285d26eb0ce273daa644ec294d953e4d859
Dan Werner <danwerner21@gmail.com>
2022-05-14 12:06:49 -0500

    MBC TMS updates

--- Tools/unix/zxcc/config.h
+++
@@ -1,9 +0,0 @@
-#define HAVE_DIRENT_H
-#define HAVE_UTIME_H
-#define HAVE_FCNTL_H
-#define HAVE_SYS_VFS_H
-#define	HAVE_UNISTD_H
-#define HAVE_STRING_H
-#define HAVE_STDLIB_H
-#define LINUX
-//#define FILETRACKER 1
