03bf1527cccca4c793127c73a3995597b84568d7
b1ackmai1er <difficultylevelhigh@gmail.com>
2022-12-15 22:02:58 +0800

    Adjust to fit on disk images

--- Source/Apps/VGM/Build.cmd
+++ Source/Apps/VGM/Build.cmd
@@ -9,4 +9,4 @@ tasm -t180 -g3 -fFF -dWBW vgmplay.asm vgmplay.com vgmplay.lst || exit /b
 tasm -t180 -g3 -fFF -dWBW ym2612.asm ym2612.com ym2612.lst || exit /b
 
 copy /Y vgmplay.com ..\..\..\Binary\Apps\ || exit /b
-copy /Y Tunes\*.* ..\..\..\Binary\Apps\Tunes\ || exit /b
+copy /Y Tunes\*.vgm ..\..\..\Binary\Apps\Tunes\ || exit /b
