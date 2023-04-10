f640630a0676072a74d118a3a4bf27fdb988b979
Wayne Warthen <wwarthen@gmail.com>
2023-04-10 16:20:12 -0700

    CPU Speed Calc Tweek
    - Exit CPU speed detection routine earlier if the clock is not ticking.
    - Fix minor regression in the SBC_simh.asm config file.

--- Tools/simh/Sim.cmd
+++ Tools/simh/Sim.cmd
@@ -2,8 +2,9 @@
 set ROM=..\..\Binary\SBC_simh.rom
 if not "%1"=="" set ROM=..\..\Binary\%1.rom
 if not exist %ROM% goto romerr
-rem start C:\Users\WWarthen\Bin\putty.exe -load "SIMH Telnet"
-start /w altairz80.exe sim.cfg %ROM%
+:: start C:\Users\WWarthen\Bin\putty.exe -load "SIMH Telnet"
+:: start /w altairz80.exe sim.cfg %ROM%
+altairz80.exe sim.cfg %ROM%
 goto :eof
 
 :romerr
