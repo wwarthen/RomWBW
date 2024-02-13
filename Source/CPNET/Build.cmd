c2721d15724da7317b29f8eec9740c16fc83be1c
Wayne Warthen <wwarthen@gmail.com>
2024-02-12 17:36:17 -0800

    More Build Tweaks

--- Source/CPNET/Build.cmd
+++ Source/CPNET/Build.cmd
@@ -6,24 +6,9 @@ set TOOLS=%~dp0..\..\Tools
 set PATH=%TOOLS%\zxcc;%PATH%
 set CPMDIR80=%TOOLS%\cpm\
 
-pushd duo\cpnet12
-zxcc nulu --O -cpn12duo "-<30" --A -*.* --L --X || exit /b
-move cpn12duo.lbr ..\.. || exit /b
-popd
-
-pushd duo\cpnet3
-zxcc nulu --O -cpn3duo "-<30" --A -*.* --L --X || exit /b
-move cpn3duo.lbr ..\.. || exit /b
-popd
-
-pushd mt011\cpnet12
-zxcc nulu --O -cpn12mt "-<30" --A -*.* --L --X || exit /b
-move cpn12mt.lbr ..\.. || exit /b
-popd
-
-pushd mt011\cpnet3
-zxcc nulu --O -cpn3mt "-<30" --A -*.* --L --X || exit /b
-move cpn3mt.lbr ..\.. || exit /b
-popd
+zxcc nulu --O -cpn12mt "-<30" --A mt011/cpnet12/*.* --L --X || exit /b
+zxcc nulu --O -cpn3mt "-<30" --A mt011/cpnet3/*.* --L --X || exit /b
+zxcc nulu --O -cpn12duo "-<30" --A duo/cpnet12/*.* --L --X || exit /b
+zxcc nulu --O -cpn3duo "-<30" --A duo/cpnet3/*.* --L --X || exit /b
 
 copy *.lbr ..\..\Binary\CPNET
