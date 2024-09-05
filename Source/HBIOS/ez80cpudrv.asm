fa6b0f1d8238dff5d3ea904702378d5b8693cc6e
Dean Netherton <dinoboards@netherton.org>
2024-09-05 21:00:31 +1000

    ez80: deprecated EZ80_UTIL_EHL_TO_HL - now using local implementation EZ80_CPY_EHL_TO_UHL

--- Source/HBIOS/ez80cpudrv.asm
+++ Source/HBIOS/ez80cpudrv.asm
@@ -77,3 +77,19 @@ EZ80_PLT_C3CYL:
 	.DB	EZ80_MEM_CYCLES
 EZ80_PLT_C2CYL:
 	.DB	EZ80_IO_CYCLES
+
+
+; ez80 helper functions/instructions
+
+_EZ80_CPY_EHL_TO_UHL:
+	PUSH	IX
+	PUSH	AF
+	.DB $5B, $DD, $21, $00, $00, $00		; LD.LIL	IX, 0
+	.DB $49, $DD, $39				; ADD.L		IX, SP
+	.DB $49, $E5					; PUSH.L	HL
+	.DB $5B, $DD, $73, $FF				; LD.LIL	(IX-1), E
+	.DB $49, $E1					; POP.L		HL
+	POP	AF
+	POP	IX
+	RET
+
