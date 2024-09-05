5c10f1881d3f928b5bd88b728406217a4c63b09a
Dean Netherton <dinoboards@netherton.org>
2024-09-05 21:16:00 +1000

    ez80: deprecated EZ80_UTIL_HL_TO_EHL - now using local implementation EZ80_CPY_UHL_TO_EHL

--- Source/HBIOS/ez80cpudrv.asm
+++ Source/HBIOS/ez80cpudrv.asm
@@ -84,12 +84,21 @@ EZ80_PLT_C2CYL:
 _EZ80_CPY_EHL_TO_UHL:
 	PUSH	IX
 	PUSH	AF
-	.DB $5B, $DD, $21, $00, $00, $00		; LD.LIL	IX, 0
-	.DB $49, $DD, $39				; ADD.L		IX, SP
-	.DB $49, $E5					; PUSH.L	HL
-	.DB $5B, $DD, $73, $FF				; LD.LIL	(IX-1), E
-	.DB $49, $E1					; POP.L		HL
+	.DB 	$5B, $DD, $21, $00, $00, $00		; LD.LIL	IX, 0
+	.DB 	$49, $DD, $39				; ADD.L		IX, SP
+	.DB 	$49, $E5				; PUSH.L	HL
+	.DB 	$5B, $DD, $73, $FF			; LD.LIL	(IX-1), E
+	.DB 	$49, $E1				; POP.L		HL
 	POP	AF
 	POP	IX
 	RET
 
+_EZ80_CPY_UHL_TO_EHL:
+	PUSH	IX
+	.DB	$5B, $DD, $21, $00, $00, $00		; LD.LIL	IX, 0
+	.DB	$49, $DD, $39				; ADD.L		IX, SP
+	.DB	$49, $E5				; PUSH.L	HL
+	.DB	$5B, $DD, $5E, $FF			; LD.LIL	E, (IX-1)
+	.DB	$49, $E1				; POP.L		HL
+	POP	IX
+	RET
