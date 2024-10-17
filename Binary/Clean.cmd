72fcee24f3f767a5057e8d67f89515eddd1ba8d8
Mark Pruden <git@mp.tidykiwi.com>
2024-10-17 15:42:41 +1100

    Publish the CPM2.2 CPM.SYS Binary Files in a new Binary/CPM22 folder

--- Binary/Clean.cmd
+++ Binary/Clean.cmd
@@ -12,6 +12,7 @@ if exist *.pdf del *.pdf
 if exist *.eeprom del *.eeprom
 
 pushd Apps && call Clean || exit /b 1 & popd
+pushd CPM22 && call Clean || exit /b 1 & popd
 pushd CPM3 && call Clean || exit /b 1 & popd
 pushd ZPM3 && call Clean || exit /b 1 & popd
 pushd CPNET && call Clean || exit /b 1 & popd
