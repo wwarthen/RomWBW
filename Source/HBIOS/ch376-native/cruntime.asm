a92bd780c8439acee8a21f0b018a974ac9e44186
Dean Netherton <dinoboards@netherton.org>
2024-11-17 09:11:08 +1100

    ch376-native: removed some intermediate files that should not have beeen committed

--- Source/HBIOS/ch376-native/cruntime.asm
+++ Source/HBIOS/ch376-native/cruntime.asm
@@ -62,3 +62,72 @@ zero_n:
 	push	de
 	pop	hl
 	ret
+
+.if 0
+; required if --optimise-for-size is targetted
+; but there appears to be a regression that stop the driver from working
+; if optimised for size is selected
+___sdcc_enter_ix:
+	ex	(sp), ix
+	push	ix
+	ld	ix, 2
+	add	ix, sp
+	ret
+
+____sdcc_lib_setmem_hl:
+l_setmem_hl:
+	ret
+
+____sdcc_load_debc_mhl:
+	ld	c, (hl)
+	inc	hl
+	ld	b, (hl)
+	inc	hl
+	ld	e, (hl)
+	inc	hl
+	ld	d, (hl)
+	ret
+
+____sdcc_4_push_hlix:
+	pop	af
+	push	af
+	push	af
+	push	af
+
+	push	de
+
+	push	ix
+	pop	de
+
+	add	hl, de
+
+	ex	de, hl
+
+	ld	hl, 2+2
+	add	hl, sp
+
+	ex	de, hl
+
+	ldi
+	ldi
+	ldi
+	ld	a, (hl)
+	ld	(de), a
+
+	inc	bc
+	inc	bc
+	inc	bc
+
+	pop	de
+	ret
+
+____sdcc_store_debc_mhl:
+	ld	(hl), c
+	inc	hl
+	ld	(hl), b
+	inc	hl
+	ld	(hl), e
+	inc	hl
+	ld	(hl), d
+	ret
+.endif
