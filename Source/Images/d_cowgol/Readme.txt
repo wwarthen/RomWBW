a8a5a85c5ce71b29cfa4b157c9e216126ea3fc95
Wayne Warthen <wwarthen@gmail.com>
2025-03-29 14:06:34 -0700

    Update Cowgol Disk Image, Issue #506
    Updated Cowgol disk image with the latest distribution from Ladislau Szilagyi.
    
    Co-Authored-By: ladislau szilagyi <87603175+Laci1953@users.noreply.github.com>

--- Source/Images/d_cowgol/Readme.txt
+++ Source/Images/d_cowgol/Readme.txt
@@ -7,11 +7,11 @@ from his GitHub repository at https://github.com/Laci1953/Cowgol_on_CP_M.
 The COWFE program included here is the RomWBW-specific version that
 is tailored to RomWBW memory management.
 
-The primary distribution site for Cowgol 2.0 is at
+Ladislau's distribution is derived from Cowgol 2.0 by David Given at
 https://github.com/davidgiven/cowgol.
 
-The user manual is available in the Doc/Language directory
-Cowgol Language.pdf
+The user manual is available in the RomWBW distribution in the
+Doc/Language directory.  The file is "Cowgol Language.pdf"
 
 The Hi-Tech C compiler components were sourced from the updated
 version by Tony Nicholson at https://github.com/agn453/HI-TECH-Z80-C.
@@ -28,7 +28,7 @@ There are two example Cowgol applications included:
   application (no assembler or C components).  The command
   line to build the application is:
 
-    COWGOL HEXDUMP.COW
+    COWGOL -M HEXDUMP.COW
 
 - DYNMSORT demonstrates a sort algorithm and is composed of
   Cowgol, C, and assembler components.  The command line to
@@ -47,7 +47,7 @@ applications which can be used as follows:
 The Adventure game program source has been added.  The command to
 build the source is:
 
-    COWGOL ADVENT.COW ADVMAIN.COW XRND.AS
+    COWGOL -O MISC.COO STRING.COO RANFILE.COO ADVENT.COW ADVTRAV.COW ADVMAIN.COW
 
 or you can use the SUBMIT file:
 
@@ -57,3 +57,8 @@ WARNING: You will need to build this application under CP/M 3 because
 COWGOL needs more main memory than is available under CP/M 2.2.
 
 -- WBW 11:43 AM 2/25/2024
+
+The Cowgol distribution has been updated based on the latest
+release by Ladislau Szilagyi as of 2/25/2025.
+
+-- WBW 1:24 PM 3/29/2025
--- Source/Images/d_cowgol/u0/MERGES.C
+++ Source/Images/d_cowgol/u0/MERGES.C
@@ -1,23 +1,23 @@
  
 int L[500], R[500];
 
-// Merges two subarrays of arr[].
-// First subarray is arr[l..m]
-// Second subarray is arr[m+1..r]
+/* Merges two subarrays of arr[].
+ First subarray is arr[l..m]
+ Second subarray is arr[m+1..r] */
 void merge(int arr[], int l, int m, int r)
 {
     int i, j, k;
     int n1 = m - l + 1;
     int n2 = r - m;
  
-    // Copy data to temp arrays L[] and R[]
+    /* Copy data to temp arrays L[] and R[] */
     for (i = 0; i < n1; i++)
         L[i] = arr[l + i];
 
     for (j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
  
-    // Merge the temp arrays back into arr[l..r
+    /* Merge the temp arrays back into arr[l..r] */
     i = 0;
     j = 0;
     k = l;
@@ -38,8 +38,7 @@ void merge(int arr[], int l, int m, int r)
         k++;
     }
  
-    // Copy the remaining elements of L[],
-    // if there are any
+    /* Copy the remaining elements of L[], if there are any */
     while (i < n1) 
     {
         arr[k] = L[i];
@@ -47,8 +46,7 @@ void merge(int arr[], int l, int m, int r)
         k++;
     }
  
-    // Copy the remaining elements of R[],
-    // if there are any
+    /* Copy the remaining elements of R[], if there are any */
     while (j < n2) 
     {
         arr[k] = R[j];
@@ -57,9 +55,9 @@ void merge(int arr[], int l, int m, int r)
     }
 }
  
-// l is for left index and r is right index of the
-// sub-array of arr to be sorted
-// first call with l = 0, r = sizeof(arr) - 1
+/* l is for left index and r is right index of the
+ sub-array of arr to be sorted
+ first call with l = 0, r = sizeof(arr) - 1 */
 void mergeSort(int arr[], int l, int r)
 {
     int m;
@@ -68,7 +66,7 @@ void mergeSort(int arr[], int l, int r)
     {
         m = l + (r - l) / 2;
  
-        // Sort first and second halves
+        /* Sort first and second halves */
         mergeSort(arr, l, m);
         mergeSort(arr, m + 1, r);
  
