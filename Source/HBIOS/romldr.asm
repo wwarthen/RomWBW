540475b50f5b93096bc54e29b72d864b93b7426a
Wayne Warthen <wwarthen@gmail.com>
2025-06-29 13:34:22 -0700

    S100 Monitor Menu Key Change (again)
    As pointed out by @kiwisincebirth, my prior choice of the numeric 1 conflicts with booting to disk units.  Changing the menu key to the letter 'O' this time.

--- Source/HBIOS/romldr.asm
+++ Source/HBIOS/romldr.asm
@@ -2666,7 +2666,7 @@ ra_ent(str_mon,	  'M',	   KY_CL, MON_BNK,   MON_IMGLOC,   MON_LOC, MON_SIZ, MON_
 ra_entsiz	.equ	$ - ra_tbl                         
 #if (BIOS == BIOS_WBW)                                     
   #if (PLATFORM == PLT_S100)                               
-ra_ent(str_smon,  '1',	   $FF,	  bid_cur,   $8000,        $8000,   $0001,   s100mon)
+ra_ent(str_smon,  'O',	   $FF,	  bid_cur,   $8000,        $8000,   $0001,   s100mon)
   #endif                                                   
 #endif                                                     
 ra_ent(str_cpm22, 'C',	   KY_BK, CPM22_BNK, CPM22_IMGLOC, CPM_LOC, CPM_SIZ, CPM_ENT)
