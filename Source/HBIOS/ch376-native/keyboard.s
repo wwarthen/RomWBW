7dbe9a5abb2008cfec33ab17463b96279264a41a
Dean Netherton <dinoboards@netherton.org>
2025-07-15 11:08:41 +1000

    ch376-native: updated readme notes and adjusted config for Z80 and eZ80 to not by default, enable the USB drivers

--- Source/HBIOS/ch376-native/keyboard.s
+++ Source/HBIOS/ch376-native/keyboard.s
@@ -5,7 +5,7 @@
 #IF (!CHNATIVEEZ80)
 #include "ch376-native/keyboard/class_hid_keyboard.c.s"
 #ENDIF
-#include "ch376-native/keyboard/kyb-init.c.s"
 #IF (!CHNATIVEEZ80)
 #include "ch376-native/keyboard/kyb_driver.c.s"
 #ENDIF
+#include "ch376-native/keyboard/kyb-init.c.s"
