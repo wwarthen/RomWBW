74cfca470da64976d6f0f30744606d51bcad8015
Wayne Warthen <wwarthen@gmail.com>
2025-09-23 11:59:47 -0700

    SZ80 Tweaks
    Support full 1MB of RAM on S100 Z80 CPU.

--- Source/SZ80/Build.cmd
+++ Source/SZ80/Build.cmd
@@ -16,7 +16,7 @@ echo.
 
 srec_cat -generate 0x0 0x100000 --constant 0x00 -o temp.dat -binary
 srec_cat temp.dat -binary -exclude 0x1B8 0x200 sz80_ptbl.bin -binary -offset 0x1B8 -o temp.dat -binary
-srec_cat temp.dat -binary -exclude 0x80000 0xE0000 ..\..\Binary\%1.rom -binary -offset 0x80000 -o temp.dat -binary
+srec_cat temp.dat -binary -exclude 0x80000 0xA0000 ..\..\Binary\%1.upd -binary -offset 0x80000 -o temp.dat -binary
 move temp.dat ..\..\Binary\%1_hd1k_prefix.dat
 
 copy /b ..\..\Binary\%1_hd1k_prefix.dat + ..\..\Binary\hd1k_cpm22.img + ..\..\Binary\hd1k_zsdos.img + ..\..\Binary\hd1k_nzcom.img + ..\..\Binary\hd1k_cpm3.img + ..\..\Binary\hd1k_zpm3.img + ..\..\Binary\hd1k_ws4.img ..\..\Binary\%1_hd1k_combo.img || exit /b
